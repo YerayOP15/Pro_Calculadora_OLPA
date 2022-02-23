b3136617cd9686ca55a9effaf95861cf48aeb973
YerayOP15 <yerayop15@gmail.com>
2022-02-23 09:38:57 +0000

    Añadiendo archvios a rama operaciones_suma_resta

--- src/main/java/Pro_Calculadora_OLPA.java
+++ src/main/java/Pro_Calculadora_OLPA.java
@@ -9,6 +9,21 @@
  */
 public class Pro_Calculadora_OLPA {
     
+    public static void Pro_Calculadora_OLPA {
+        /* Declara dos variables de tipo int (entero) (con el valor que desees), muestra
+        por consola la suma +, resta -, multiplicación * y división /.*/
+
+        
+        int num1 = 5;
+        int num2 = 12;
+        
+        System.out.println ("La suma es " + (num1 + num2));
+        System.out.println ("La resta es " + (num1 - num2));
+    }
     
+    public static void main (String[] args) {
+        // TODO code application logic here
+       
+        Pro_Calculadora_OLPA();
     
 }
